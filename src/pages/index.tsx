e1c9567e29f6f793b30c71a4e936054fb6c448af
TeriLuna <catherinelozada7@gmail.com>
2023-05-25 12:19:16 -0500

    Adding information updated by user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import About from '../components/About';
 import Analytics from '../components/Analytics';
 import Canvas from '../components/Canvas';
 import Features from '../components/Features';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import LazyShow from '../components/LazyShow';
 import MainHero from '../components/MainHero';
 import MainHeroImage from '../components/MainHeroImage';
 import Pricing from '../components/Pricing';
-import Product from '../components/Product';
 
 const App = () => {
   return (
@@ -28,7 +28,7 @@ const App = () => {
       <Canvas />
       <LazyShow>
         <>
-          <Product />
+          <About />
           <Canvas />
         </>
       </LazyShow>
@@ -44,7 +44,7 @@ const App = () => {
       <LazyShow>
         <>
           <Canvas />
-          <About />
+          <Footer />
         </>
       </LazyShow>
       <Analytics />
